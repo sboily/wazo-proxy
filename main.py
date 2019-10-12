4f0e6c4b2d4d4b27faba00898e175e843dd98600
Sylvain Boily <sylvain@wazo.io>
2019-10-12 11:48:10 -0400

    fix

--- main.py
+++ main.py
@@ -65,7 +65,9 @@ async def send_websocket_fake(websocket, queue):
 
 @app.get("{full_path:path}")
 def read_all(full_path: str, request: Request):
-    params = request.get('query_params')
+    params = None
+    if request.query_params:
+        params = request.query_params
     r = requests.get('https://{}/{}'.format(server, full_path), params=params, headers=request.headers)
     try:
         print(r.json())
